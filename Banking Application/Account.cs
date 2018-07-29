5837fc1142a0300e54c85e3975ef57e962675ebd
Neha Anand <nanand@tavisca.com>
2018-07-29 17:20:42 +0530

    ADO Complete

---
+++ Banking Application/Account.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DBConnect;
+using System.Data.SqlClient;
+namespace Banking_Application
+{
+    class Account:IDatabaseInterface
+    {
+        static Database ob = new Database();
+        public void insert(int id,string name,int accType)
+        {
+            SqlConnection conn = ob.connect();
+            string sqlQuery = "insert into users values(" + id + "," + name + "," + accType + ","+0+")";
+            SqlCommand cmd = new SqlCommand(sqlQuery, conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+        public void disp()
+        {
+            SqlConnection conn = ob.connect();
+            string sqlQuery = "select * from users";
+            SqlCommand cmd = new SqlCommand(sqlQuery, conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while(reader.NextResult())
+            {
+                Console.WriteLine("Id:"+reader.GetValue(0));
+                Console.WriteLine("Name:" + reader.GetValue(1));
+                Console.WriteLine("Account Type:" + reader.GetValue(2));
+                Console.WriteLine("Total Amount:" + reader.GetValue(3));
+            }
+            conn.Close();
+        }
+        public void searchById(int id)
+        {
+            SqlConnection conn = ob.connect();
+            string sqlQuery = "select * from users where id=" + id;
+            SqlCommand cmd = new SqlCommand(sqlQuery, conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.NextResult())
+            {
+                Console.WriteLine("Id:" + reader.GetValue(0));
+                Console.WriteLine("Name:" + reader.GetValue(1));
+                Console.WriteLine("Account Type:" + reader.GetValue(2));
+                Console.WriteLine("Total Amount:" + reader.GetValue(3));
+            }
+            
+            conn.Close();
+        }
+        public void depositAmount(int id,int amount)
+        {
+            SqlConnection conn = ob.connect();
+            string sqlQuery = "update users set balance=balance+" + amount + " where id=" + id;
+            SqlCommand cmd = new SqlCommand(sqlQuery, conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+        public void WithdrawlAmount(int id, int amount)
+        {
+            SqlConnection conn = ob.connect();
+            string sqlQuery = "select * from users where id=" + id + "";
+            SqlCommand cmd = new SqlCommand(sqlQuery, conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            int check=0;
+            if (reader.NextResult())
+            {
+                int balance = (int)reader.GetValue(3);
+                int accType = (int)reader.GetValue(2);
+                if (accType == 1 && balance < 1000)
+                    Console.WriteLine("Insufuccient balance you can not withdraw money");
+                else if (accType == 2 && balance < 0)
+                    Console.WriteLine("Insufuccient balance you can not withdraw money");
+                else if (accType == 3 && balance < -10000)
+                    Console.WriteLine("Insufuccient balance you can not withdraw money");
+                else
+                    check = 1;
+            }
+            conn.Close();
+            conn = ob.connect();
+            if (check == 1)
+            {
+                string sqlQueryNew = "update users set balance=balance-" + amount + " where id=" + id;
+                SqlCommand cmdNew = new SqlCommand(sqlQuery, conn);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+        }
+        public void CalculateInterest(int id)
+        {
+            SqlConnection conn = ob.connect();
+            string sqlQuery = "select * from users where id="+id+"";
+            SqlCommand cmd = new SqlCommand(sqlQuery, conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            if(reader.NextResult())
+            {
+                int amount = (int)reader.GetValue(3);
+                int accType = (int)reader.GetValue(2);
+                if (accType == 1)
+                    Console.WriteLine("Calculated Interest=" + amount * 4 / 100);
+                else if (accType == 2)
+                    Console.WriteLine("Calculated Interest=" + amount * 1 / 100);
+                else
+                    Console.WriteLine("Invalid Account Type");
+            }
+            conn.Close();
+        }
+
+    }
+}
+
+
---
+++ Banking Application/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking_Application
+{
+    class Program
+    {
+
+        static Account account = new Account();
+        static void Main(string[] args)
+        {
+            
+            int accountId,amount,accountType;
+            string customerName;
+          
+          
+             Console.WriteLine("Enter Number of Records to be Added");
+            int noofrecords = int.Parse(Console.ReadLine());
+            for (int index = 0; index < noofrecords; index++)
+            {
+                Console.WriteLine("Enter {0} Account Id",index+1);
+                accountId = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter {0} Customer Name",index+1);
+                customerName = Console.ReadLine();
+                Console.WriteLine("Enter Account Type:-\n  1. Saving\n 2. Current\n 3. DMAT");
+                accountType = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+                account.insert(accountId, customerName, accountType);
+                Console.WriteLine("Data Inserted");
+            }
+            int flag = 1;
+            do
+            {
+                Console.Write("Enter \n 1. To check Account Details\n 2. Search by Account ID \n 3. To deposit money\n 4. To withdraw money\n 5. To Calulate Interest on an account \n");
+                int choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        account.disp();            //Reference parameter passed
+                        break;
+                    case 2:
+                        int accountNumber = int.Parse(Console.ReadLine());
+                        account.searchById(accountNumber);
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter Id");
+                        accountNumber=int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter amount");
+                        amount = int.Parse(Console.ReadLine());
+                        account.depositAmount(accountNumber,amount);
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter Id");
+                        accountNumber=int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter amount");
+                        amount = int.Parse(Console.ReadLine());
+                        account.WithdrawlAmount(accountNumber,amount);
+                        break;
+                    case 5:
+                        Console.WriteLine("Enter Id");
+                        accountNumber = int.Parse(Console.ReadLine());
+                        account.CalculateInterest(accountNumber);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Entry!");
+                        break;
+
+                }
+                Console.WriteLine("Enter 1 to Continue and 0 To Stop :- ");
+                Console.WriteLine();
+                flag = int.Parse(Console.ReadLine());
+            }
+            while (flag == 1);
+            Console.ReadKey();
+        }
+    }
+}
